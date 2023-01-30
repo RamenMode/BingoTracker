5ec9c26248846271dcf2db1fe0b9c24b2eda49cb
RamenMode <60017278+RamenMode@users.noreply.github.com>
2023-01-30 12:37:10 -0500

    attempted to clean user input data, unstable commit

--- src/Boards.js
+++ src/Boards.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import BingoBoard from './BingoBoard';
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Boards(props) {
+    const navigation = useNavigate();
+    const [BingoData, setBingoData] = useState('')
+    const [BingoArray, setBingoArray] = useState([])
+//fix await document.getElementById. LIkely change to a form instead. 
+    async function onPress() {
+        const data = await document.getElementById("numbers").value
+        setBingoData(data)
+        let Spaceless = BingoData.replace(/\s/g, '');
+        let arrayBingo = Spaceless.split(',');
+        setBingoArray(arrayBingo)
+        console.log(BingoArray)
+        alert(BingoArray)
+    }
+
     return (
         <div>
             <div className="App">
@@ -12,10 +27,10 @@ function Boards(props) {
                 Track multiple sheets at the same time. Get started below.
             </h4>
             <p>Please enter your bingo sheet numbers from left to right, separated by commas starting with the first row ex. 1, 16, 37, 55, 75, 2, 24...</p>
-            <label for="Name">Numbers:</label>
-            <input type="text" id="Name" name="Name" placeholder="enter your board"></input>
+            <label for="numbers">Numbers:</label>
+            <input type="text" id="numbers" name="numbers" placeholder="enter your board"></input>
             <p></p>
-            <button>Create a New Sheet +</button>
+            <button className="custom-button blue" onClick={() => {onPress()}}>Continue</button>
             </header>
             </div>
         </div>
