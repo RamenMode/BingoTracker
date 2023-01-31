c538fe85c5921ce05eff6c0ee355960d499bd95b
RamenMode <60017278+RamenMode@users.noreply.github.com>
2023-01-31 14:14:28 -0500

    app complete

--- src/BingoBoard.js
+++ src/BingoBoard.js
@@ -8,31 +8,31 @@ function BingoBoard(props) {
         <div>
             <h1>BINGO</h1>
             <section class="grid-1">
-                <div class="item-1">3</div>
-                <div class="item-2">19</div>
-                <div class="item-3">32</div>
-                <div class="item-4">46</div>
-                <div class="item-5">68</div>
-                <div class="item-6">6</div>
-                <div class="item-7">22</div>
-                <div class="item-8">31</div>
-                <div class="item-9">54</div>
-                <div class="item-10">75</div>
-                <div class="item-10">14</div>
-                <div class="item-10">26</div>
-                <div class="item-10">38</div>
-                <div class="item-10">58</div>
-                <div class="item-10">62</div>
-                <div class="item-10">11</div>
-                <div class="item-10">29</div>
-                <div class="item-10">45</div>
-                <div class="item-10">57</div>
-                <div class="item-10">73</div>
-                <div class="item-10">1</div>
-                <div class="item-10">16</div>
-                <div class="item-10">35</div>
-                <div class="item-10">48</div>
-                <div class="item-10">61</div>
+                {props.num1t ? <div class="item-1">{props.num1}</div>:<div class="item-2">{props.num1}</div>}
+                {props.num2t ? <div class="item-1">{props.num2}</div>:<div class="item-2">{props.num2}</div>}
+                {props.num3t ? <div class="item-1">{props.num3}</div>:<div class="item-2">{props.num3}</div>}
+                {props.num4t ? <div class="item-1">{props.num4}</div>:<div class="item-2">{props.num4}</div>}
+                {props.num5t ? <div class="item-1">{props.num5}</div>:<div class="item-2">{props.num5}</div>}
+                {props.num6t ? <div class="item-1">{props.num6}</div>:<div class="item-2">{props.num6}</div>}
+                {props.num7t ? <div class="item-1">{props.num7}</div>:<div class="item-2">{props.num7}</div>}
+                {props.num8t ? <div class="item-1">{props.num8}</div>:<div class="item-2">{props.num8}</div>}
+                {props.num9t ? <div class="item-1">{props.num9}</div>:<div class="item-2">{props.num9}</div>}
+                {props.num10t ? <div class="item-1">{props.num10}</div>:<div class="item-2">{props.num10}</div>}
+                {props.num11t ? <div class="item-1">{props.num11}</div>:<div class="item-2">{props.num11}</div>}
+                {props.num12t ? <div class="item-1">{props.num12}</div>:<div class="item-2">{props.num12}</div>}
+                {props.num13t ? <div class="item-1">{props.num13}</div>:<div class="item-2">{props.num13}</div>}
+                {props.num14t ? <div class="item-1">{props.num14}</div>:<div class="item-2">{props.num14}</div>}
+                {props.num15t ? <div class="item-1">{props.num15}</div>:<div class="item-2">{props.num15}</div>}
+                {props.num16t ? <div class="item-1">{props.num16}</div>:<div class="item-2">{props.num16}</div>}
+                {props.num17t ? <div class="item-1">{props.num17}</div>:<div class="item-2">{props.num17}</div>}
+                {props.num18t ? <div class="item-1">{props.num18}</div>:<div class="item-2">{props.num18}</div>}
+                {props.num19t ? <div class="item-1">{props.num19}</div>:<div class="item-2">{props.num19}</div>}
+                {props.num20t ? <div class="item-1">{props.num20}</div>:<div class="item-2">{props.num20}</div>}
+                {props.num21t ? <div class="item-1">{props.num21}</div>:<div class="item-2">{props.num21}</div>}
+                {props.num22t ? <div class="item-1">{props.num22}</div>:<div class="item-2">{props.num22}</div>}
+                {props.num23t ? <div class="item-1">{props.num23}</div>:<div class="item-2">{props.num23}</div>}
+                {props.num24t ? <div class="item-1">{props.num24}</div>:<div class="item-2">{props.num24}</div>}
+                {props.num25t ? <div class="item-1">{props.num25}</div>:<div class="item-2">{props.num25}</div>}
                 </section>
             </div>
     );
--- src/Boards.js
+++ src/Boards.js
@@ -4,19 +4,44 @@ import BingoBoard from './BingoBoard';
 import { Navigate, useNavigate } from "react-router-dom";
 
 function Boards(props) {
+    
     const navigation = useNavigate();
     const [BingoArray, setBingoArray] = useState([])
+    const [BingoArrayTruth, setBingoArrayTruth] = useState([])
     const [Input, setInput] = useState('');
+    const [CalledNumber, setCalledNumber] = useState('');
+    var index = -1;
+
+    const [, updateState] = React.useState();
+    const forceUpdate = React.useCallback(() => updateState({}), []);
 
     const handleChange = (event) => {
         // input value from "event"
-        setInput(event.target.value);
-      };
+        setInput(event.target.value); 
+    };
+    
+    const handleCalledNumber = (event) => {
+        setCalledNumber(event.target.value); 
+    };
     //fix await document.getElementById. LIkely change to a form instead. 
     async function onPress(data) {
+        setInput("")
         let Spaceless = data.replace(/\s/g, '');
         let arrayBingo = Spaceless.split(',');
-        setBingoArray(arrayBingo)
+        setBingoArray([...BingoArray, arrayBingo])
+        setBingoArrayTruth([...BingoArrayTruth, Array.apply(true, Array(25)).map(function () {return true})])
+    }
+    async function onPressCalled(data) {
+        setCalledNumber("")
+        let BingoArrayTruthTemp = BingoArrayTruth
+        for (let i=0; i<BingoArray.length; i++) {
+            for (let x=0; x<BingoArray[i].length; x++) {
+                if (data == BingoArray[i][x]) {
+                    BingoArrayTruthTemp[i][x] = false;
+                }
+            }
+        }
+        setBingoArrayTruth(BingoArrayTruthTemp)
     }
 
     useEffect(() => console.log(BingoArray), [BingoArray])
@@ -25,15 +50,106 @@ function Boards(props) {
         <div>
             <div className="App">
             <header className="App-header">
-            <BingoBoard/>
+            <BingoBoard
+                num1 = {Math.floor((Math.random()*15)+1)}
+                num2 = {Math.floor((Math.random()*15)+16)}
+                num3 = {Math.floor((Math.random()*15)+31)}
+                num4 = {Math.floor((Math.random()*15)+46)}
+                num5 = {Math.floor((Math.random()*15)+61)}
+                num6 = {Math.floor((Math.random()*15)+1)}
+                num7 = {Math.floor((Math.random()*15)+16)}
+                num8 = {Math.floor((Math.random()*15)+31)}
+                num9 = {Math.floor((Math.random()*15)+46)}
+                num10 = {Math.floor((Math.random()*15)+61)}
+                num11 = {Math.floor((Math.random()*15)+1)}
+                num12 = {Math.floor((Math.random()*15)+16)}
+                num13 = {Math.floor((Math.random()*15)+31)}
+                num14 = {Math.floor((Math.random()*15)+46)}
+                num15 = {Math.floor((Math.random()*15)+61)}
+                num16 = {Math.floor((Math.random()*15)+1)}
+                num17 = {Math.floor((Math.random()*15)+16)}
+                num18 = {Math.floor((Math.random()*15)+31)}
+                num19 = {Math.floor((Math.random()*15)+46)}
+                num20 = {Math.floor((Math.random()*15)+61)}
+                num21 = {Math.floor((Math.random()*15)+1)}
+                num22 = {Math.floor((Math.random()*15)+16)}
+                num23 = {Math.floor((Math.random()*15)+31)}
+                num24 = {Math.floor((Math.random()*15)+46)}
+                num25 = {Math.floor((Math.random()*15)+61)}
+            />
             <h4>
                 Track multiple sheets at the same time. Get started below.
             </h4>
             <p>Please enter your bingo sheet numbers from left to right, separated by commas starting with the first row ex. 1, 16, 37, 55, 75, 2, 24...</p>
             <label for="numbers">Numbers:</label>
-            <input onChange={handleChange} type="text" id="numbers" name="numbers" placeholder="enter your board"></input>
+            <input value = {Input} onChange={handleChange} type="text" id="numbers" name="numbers" placeholder="enter your board"></input>
             <p></p>
             <button className="custom-button blue" onClick={() => {onPress(Input)}}>Continue</button>
+            <p></p>
+            <label for="called">Called number: </label>
+            <input value = {CalledNumber} onChange = {handleCalledNumber} type = "text" id="called" name="called" placeholder="enter one number"></input>
+            <p></p>
+            <button className="custom-button blue" onClick={() => {onPressCalled(CalledNumber)}}>Continue</button>
+            <p></p>
+            <button onClick={forceUpdate}>Manually Update</button>
+            {BingoArray.map((board) => {
+                index = index + 1;
+                return(
+                    <BingoBoard
+                        num1t = {BingoArrayTruth[index][0]}
+                        num2t = {BingoArrayTruth[index][1]}
+                        num3t = {BingoArrayTruth[index][2]}
+                        num4t = {BingoArrayTruth[index][3]}
+                        num5t = {BingoArrayTruth[index][4]}
+                        num6t = {BingoArrayTruth[index][5]}
+                        num7t = {BingoArrayTruth[index][6]}
+                        num8t = {BingoArrayTruth[index][7]}
+                        num9t = {BingoArrayTruth[index][8]}
+                        num10t = {BingoArrayTruth[index][9]}
+                        num11t = {BingoArrayTruth[index][10]}
+                        num12t = {BingoArrayTruth[index][11]}
+                        num13t = {BingoArrayTruth[index][12]}
+                        num14t = {BingoArrayTruth[index][13]}
+                        num15t = {BingoArrayTruth[index][14]}
+                        num16t = {BingoArrayTruth[index][15]}
+                        num17t = {BingoArrayTruth[index][16]}
+                        num18t = {BingoArrayTruth[index][17]}
+                        num19t = {BingoArrayTruth[index][18]}
+                        num20t = {BingoArrayTruth[index][19]}
+                        num21t = {BingoArrayTruth[index][20]}
+                        num22t = {BingoArrayTruth[index][21]}
+                        num23t = {BingoArrayTruth[index][22]}
+                        num24t = {BingoArrayTruth[index][23]}
+                        num25t = {BingoArrayTruth[index][24]}
+                        num1 = {board[0]}
+                        num2 = {board[1]}
+                        num3 = {board[2]}
+                        num4 = {board[3]}
+                        num5 = {board[4]}
+                        num6 = {board[5]}
+                        num7 = {board[6]}
+                        num8 = {board[7]}
+                        num9 = {board[8]}
+                        num10 = {board[9]}
+                        num11 = {board[10]}
+                        num12 = {board[11]}
+                        num13 = {board[12]}
+                        num14 = {board[13]}
+                        num15 = {board[14]}
+                        num16 = {board[15]}
+                        num17 = {board[16]}
+                        num18 = {board[17]}
+                        num19 = {board[18]}
+                        num20 = {board[19]}
+                        num21 = {board[20]}
+                        num22 = {board[21]}
+                        num23 = {board[22]}
+                        num24 = {board[23]}
+                        num25 = {board[24]}
+                    >
+                    </BingoBoard>
+                )
+            })}
             </header>
             </div>
         </div>
